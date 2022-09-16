1d44dca7b911ee95eff7d767d1e8f08328a332d4
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-09-15 22:36:31 -0400

    added higher basin function and updated h and main files

--- main.cpp
+++ main.cpp
@@ -9,5 +9,9 @@ int main()
   	std::cout << get_east_storage("09/28/2018") << " billion gallons" << std::endl;
 	std::cout << "Min East Storage: " << get_min_east() << " billion gallons" << std::endl;
 	std::cout << "Max East Storage: " << get_max_east() << " billion gallons" << std::endl;
+	std::cout << "Higher Basin on Date: " << compare_basins("09/13/2018") << std::endl;
+	std::cout << "Higher Basin on Date: " << compare_basins("05/23/2018") << std::endl;
+	std::cout << "Higher Basin on Date: " << compare_basins("01/05/2018") << std::endl;
+	std::cout << "Higher Basin on Date: " << compare_basins("07/23/2018") << std::endl;
   	return 0;
 }
